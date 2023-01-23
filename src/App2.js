acf2b7c875935175fe4cf30bb7599196e1d454ff
Nixgit2000 <fourwaycoordination@gmail.com>
2023-01-23 13:41:52 -0500

    commit

---
+++ src/App2.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App2() {
+  return (
+    <div className="App2">
+<h3>Dev stuff...</h3>
+    </div>
+  );
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import App2 from './App2';
+import App3 from './App3';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <div>
     <App />
+    <App2 /><App3 />
+    
+  </div>
   </React.StrictMode>
 );
 
